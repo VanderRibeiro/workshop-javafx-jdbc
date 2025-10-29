18b27b716008e34619390eec4d8f60b4db7ab263
unknown <vbruno383@gmail.com>
2025-10-28 23:23:34 -0300

    Adicionando acesso ao banco de dados

--- src/module-info.java
+++ src/module-info.java
@@ -1,8 +1,14 @@
 module workshop_javafx_jdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires java.sql;
+	requires java.desktop;
+	requires javafx.base;
+
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens gui to javafx.fxml;
 	opens model.entities to javafx.base;
+	exports model.services;
+    exports gui;
 }
